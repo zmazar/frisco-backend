49184a0fd9255990de8e3d691254aae95d7abb4e
Zach Mazar <zmazar@narrator.friskytaphouse.com>
2016-10-23 11:58:17 -0400

    Fixed up init scripts

--- update.sh
+++ update.sh
@@ -7,7 +7,7 @@ git pull
 # Install the init.d script and setup permissions
 cp ./service/friscod /etc/init.d/friscod
 chmod 755 /etc/init.d/friscod
-chmod root:root /etc/init.d/friscod
+chown root:root /etc/init.d/friscod
 
 # Add it as a service that starts on boot
 update-rc.d friscod defaults
